eacf70986009da911f59eefc3212503dfa3f4a3f
Evgeniy Panchenko <panchenko.ten@gmail.com>
2021-08-18 18:44:46 +0300

    Setup code

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+enum CurrentLight {
+    case red, yellow, green
 }
 
+class ViewController: UIViewController {
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var startButton: UIButton!
+    
+    var currentLight = CurrentLight.red
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        redLight.alpha = 0.3
+        yellowLight.alpha = 0.3
+        greenLight.alpha = 0.3
+    }
+        
+    @IBAction func startButtonPressed() {
+        startButton.setTitle("NEXT", for: .normal)
+        
+        switch currentLight {
+        
+        case .red:
+            greenLight.alpha = 0.3
+            redLight.alpha = 1
+            currentLight = .yellow
+        case .yellow:
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1
+            currentLight = .green
+        case .green:
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 1
+            currentLight = .red
+        }
+    }
+}
